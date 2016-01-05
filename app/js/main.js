fd69911167b87a94cc8540e5a9b3315c991eff6c
Kevin Mooney <kmooney517@gmail.com>
2016-01-05 15:38:07 -0500

    struggling with adding markers - location is being saved in array... but no image of marker is showing on map

--- app/js/main.js
+++ app/js/main.js
@@ -33,14 +33,36 @@ module.exports = exports['default'];
 Object.defineProperty(exports, '__esModule', {
   value: true
 });
+
 var Controller = function Controller($scope) {
 
-  $scope.map = {
+  var vm = this;
+
+  vm.map = {
     center: {
       latitude: 33.7482665,
       longitude: -84.389121
     },
-    zoom: 10
+    zoom: 10,
+    markers: [],
+    events: {
+      click: function click(map, eventName, originalEventArgs) {
+        var e = originalEventArgs[0];
+        var lat = e.latLng.lat(),
+            lon = e.latLng.lng();
+        var marker = {
+          id: Date.now(),
+          coords: {
+            latitude: lat,
+            longitude: lon
+          },
+          icon: '{url:    "developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png" }'
+        };
+        vm.map.markers.push(marker);
+        console.log(vm.map.markers);
+        $scope.$apply();
+      }
+    }
   };
 };
 
--- js/controller.js
+++ js/controller.js
@@ -1,13 +1,36 @@
+
 let Controller = function($scope) {
   
 
-  $scope.map = { 
+
+  let vm = this;
+
+  vm.map = { 
     center: { 
       latitude: 33.7482665, 
       longitude: -84.389121
     }, 
-    zoom: 10
+    zoom: 10,
+    markers: [],
+    events: {
+      click: function (map, eventName, originalEventArgs) {
+        var e = originalEventArgs[0];
+        var lat = e.latLng.lat(),lon = e.latLng.lng();
+        var marker = {
+          id: Date.now(),
+          coords: {
+            latitude: lat,
+            longitude: lon
+          },
+          icon: '{url:    "developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png" }',
+        };
+        vm.map.markers.push(marker);
+        console.log(vm.map.markers);
+        $scope.$apply();
+      }
+    }
   };
+
 
 
 };
